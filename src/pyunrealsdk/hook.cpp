a787807dbf43fe11a29162c3824c0d7799ed4636
apple1417 <git@apple1417.dev>
2023-07-10 21:00:57 +1200

    support overwriting returns

--- src/pyunrealsdk/hook.cpp
+++ src/pyunrealsdk/hook.cpp
@@ -2,70 +2,155 @@
 #include "pyunrealsdk/hooks.h"
 #include "pyunrealsdk/logging.h"
 #include "unrealsdk/hook_manager.h"
+#include "unrealsdk/unreal/cast_prop.h"
+#include "unrealsdk/unreal/prop_traits.h"
+#include "unrealsdk/unreal/wrappers/property_proxy.h"
+
+using namespace unrealsdk::unreal;
+using namespace unrealsdk::hook_manager;
 
 namespace pyunrealsdk::hooks {
+
+namespace {
+
+// Sentinel class for python.
+struct Block {};
+
+/**
+ * @brief Handles calling a pre-hook.
+ *
+ * @param hook The hook details.
+ * @param callback The python hook callback.
+ * @return True if to block the function call.
+ */
+bool handle_prehook(Details& hook, const py::function& callback) {
+    auto ret = callback(hook.obj, hook.args /*, hook.func*/);
+
+    if (py::isinstance<py::tuple>(ret)) {
+        auto ret_tuple = py::cast<py::tuple>(ret);
+        auto size = ret_tuple.size();
+
+        if (size == 1) {
+            LOG(WARNING,
+                "Return value of pre-hook was tuple of size 1, expected 2. Not overwriting return "
+                "value.");
+        } else {
+            if (size != 2) {
+                LOG(WARNING,
+                    "Return value of pre-hook was tuple of size {}, expected 2. Extra values will "
+                    "be ignored.",
+                    size);
+            }
+
+            auto ret_override = ret_tuple[1];
+            cast_prop(hook.ret.prop, [&hook, &ret_override]<typename T>(const T* /*prop*/) {
+                auto value = py::cast<typename PropTraits<T>::Value>(ret_override);
+                hook.ret.set<T>(value);
+            });
+        }
+
+        py::object execution_state = ret_tuple[0];
+        return py::isinstance<Block>(execution_state)
+               || execution_state.equal(py::type::of<Block>());
+    }
+
+    return py::isinstance<Block>(ret) || ret.equal(py::type::of<Block>());
+}
+
+/**
+ * @brief Handles calling a post-hook.
+ *
+ * @param hook The hook details.
+ * @param callback The python hook callback.
+ */
+void handle_posthook(Details& hook, const py::function& callback) {
+    py::object ret;
+    if (hook.ret.has_value()) {
+        cast_prop(hook.ret.prop, [&hook, &ret]<typename T>(const T* /*prop*/) {
+            ret = py::cast(hook.ret.get<T>());
+        });
+    } else {
+        ret = py::ellipsis{};
+    }
+
+    callback(hook.obj, hook.args /*, hook.func*/, ret);
+}
+
+}  // namespace
 
 void register_module(py::module_& mod) {
     auto hooks = mod.def_submodule("hooks");
 
-    py::enum_<unrealsdk::hook_manager::Type>(hooks, "Type", "Enum of possible hook types.")
-        .value("PRE", unrealsdk::hook_manager::Type::PRE,
-               "Called before running the hooked function.")
-        .value("POST", unrealsdk::hook_manager::Type::POST,
+    py::enum_<Type>(hooks, "Type", "Enum of possible hook types.")
+        .value("PRE", Type::PRE, "Called before running the hooked function.")
+        .value("POST", Type::POST,
                "Called after the hooked function, but only if it was allowed to run.")
-        .value("POST_UNCONDITIONAL", unrealsdk::hook_manager::Type::POST_UNCONDITIONAL,
+        .value("POST_UNCONDITIONAL", Type::POST_UNCONDITIONAL,
                "Called after the hooked function, even if it got blocked.");
 
-    hooks.def(
-        "log_all_calls", &unrealsdk::hook_manager::log_all_calls,
-        "Toggles logging all unreal function calls. Best used in short bursts for debugging.\n"
-        "\n"
-        "Args:\n"
-        "    should_log True to turn on logging all calls, false to turn it off.",
-        "should_log"_a);
+    py::class_<Block>(
+        hooks, "Block",
+        "A sentinel used to indicate a hook should block execution of the unrealscript\n"
+        "function.")
+        .def(py::init<>());
 
-    hooks.def("inject_next_call", &unrealsdk::hook_manager::inject_next_call,
+    hooks.def("log_all_calls", &log_all_calls,
+              "Toggles logging all unreal function calls. Best used in short bursts for\n"
+              "debugging.\n"
+              "\n"
+              "Args:\n"
+              "    should_log True to turn on logging all calls, false to turn it off.",
+              "should_log"_a);
+
+    hooks.def("inject_next_call", &inject_next_call,
               "Makes the next unreal function call completely ignore hooks.\n"
               "\n"
               "Typically used to avoid recursion when re-calling the hooked function.");
 
     hooks.def(
         "add_hook",
-        [](const std::wstring& func, unrealsdk::hook_manager::Type type,
-           const std::wstring& identifier, const py::function& callback) {
-            unrealsdk::hook_manager::add_hook(
-                func, type, identifier, [callback](unrealsdk::hook_manager::Details& hook) {
-                    try {
-                        py::gil_scoped_acquire gil{};
-                        return (bool)py::bool_(callback(hook.obj, hook.args, hook.ret, hook.func));
-                    } catch (const std::exception& ex) {
-                        logging::log_python_exception(ex);
+        [](const std::wstring& func, Type type, const std::wstring& identifier,
+           const py::function& callback) {
+            add_hook(func, type, identifier, [type, callback](Details& hook) {
+                try {
+                    py::gil_scoped_acquire gil{};
+
+                    if (type == Type::PRE) {
+                        return handle_prehook(hook, callback);
                     }
+
+                    handle_posthook(hook, callback);
                     return false;
-                });
+
+                } catch (const std::exception& ex) {
+                    logging::log_python_exception(ex);
+                    return false;
+                }
+            });
         },
         "Adds a hook.\n"
         "\n"
-        "Hook callbacks take four positional args. These are, in order:\n"
+        "Pre-hook callbacks take three positional args. These are, in order:\n"
         "    obj: The object the hooked function was called on.\n"
         "    args: The arguments the hooked function was called with. Note that while\n"
         "          this is mutable, modifying it will *not* modify the actual function\n"
         "          arguments.\n"
-        "    ret: A proxy for the return value.\n"
-        "         During pre-hooks, it's an unset value (Ellipsis), and setting it will\n"
-        "         overwrite the return value of the function call. Whatever value is set\n"
-        "         when pre-hook processing is complete will be used, if multiple hooks\n"
-        "         all want to overwrite it they will need to cooperate.\n"
-        "         During post-hooks, it's set to the return value of the unreal function.\n"
-        "         This may have been overwritten by a pre-hook. If execution was blocked\n"
-        "         and it was not overwritten during a pre-hook, it will still be unset.\n"
-        "         Note that while there may be a `ReturnValue` property in the args\n"
-        "         struct, it is not necessarily correct, this always will be.\n"
         "    func: The function which was called, bound to the same object. Can be used\n"
         "          to re-call it.\n"
-        "Pre-hooks should return if to block execution - if any pre-hook returns true,\n"
-        "the unreal function will not be run.\n"
-        "The return value is ignored for the other hook types.\n"
+        "Pre-hooks may return the special `Block` value to block execution of the unreal\n"
+        "function. Any other value will allow the function to continue.\n"
+        "Additionally, if it returns a 2-tuple, the first entry will be used as above,\n"
+        "while the second entry will overwrite the function's return value. Suggest using\n"
+        "Ellipsis for the first arg if you want to allow the function to continue while\n"
+        "overwriting it's return.\n"
+        "\n"
+        "Post-hook callbacks take a fourth positional arg:\n"
+        "    ret: The return value of the unreal function. This may have been overwritten\n"
+        "         by a pre-hook. If execution was blocked, and it was not overwritten, it\n"
+        "         it will be set to Ellipsis.\n"
+        "         Note that while there may be a `ReturnValue` property in the args\n"
+        "         struct, it is not necessarily correct, this always will be.\n"
+        "Post-hook return values are ignored.\n"
         "\n"
         "Args\n"
         "    func: The function to hook.\n"
