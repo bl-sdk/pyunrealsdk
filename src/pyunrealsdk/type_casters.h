3afc98636c6426d8a751a4cc1ee446bf7a1cda67
apple1417 <git@apple1417.dev>
2023-07-15 14:09:51 +1200

    downcast uobject children

--- src/pyunrealsdk/type_casters.h
+++ src/pyunrealsdk/type_casters.h
@@ -4,10 +4,87 @@
 // This file is included in the pch, so we can't include it here, just need to assume we have access
 // to everything we need already
 
+#include "unrealsdk/unreal/class_name.h"
+#include "unrealsdk/unreal/classes/properties/copyable_property.h"
+#include "unrealsdk/unreal/classes/properties/uarrayproperty.h"
+#include "unrealsdk/unreal/classes/properties/uboolproperty.h"
+#include "unrealsdk/unreal/classes/properties/uclassproperty.h"
+#include "unrealsdk/unreal/classes/properties/uinterfaceproperty.h"
+#include "unrealsdk/unreal/classes/properties/uobjectproperty.h"
+#include "unrealsdk/unreal/classes/properties/ustrproperty.h"
+#include "unrealsdk/unreal/classes/properties/ustructproperty.h"
+#include "unrealsdk/unreal/classes/ufield.h"
+#include "unrealsdk/unreal/classes/ufunction.h"
+#include "unrealsdk/unreal/classes/uobject.h"
+#include "unrealsdk/unreal/classes/uproperty.h"
+#include "unrealsdk/unreal/classes/uscriptstruct.h"
+#include "unrealsdk/unreal/classes/ustruct.h"
 #include "unrealsdk/unreal/structs/fname.h"
 
-namespace pybind11::detail {
+namespace pybind11 {
 
+// Make the UObject hierarchy automatically downcast
+namespace {
+
+using known_uobject_children = std::tuple<unrealsdk::unreal::UObject,
+                                          unrealsdk::unreal::UField,
+                                          unrealsdk::unreal::UStruct,
+                                          unrealsdk::unreal::UClass,
+                                          unrealsdk::unreal::UFunction,
+                                          unrealsdk::unreal::UScriptStruct,
+                                          unrealsdk::unreal::UProperty,
+                                          unrealsdk::unreal::UInt8Property,
+                                          unrealsdk::unreal::UInt16Property,
+                                          unrealsdk::unreal::UIntProperty,
+                                          unrealsdk::unreal::UInt64Property,
+                                          unrealsdk::unreal::UByteProperty,
+                                          unrealsdk::unreal::UUInt16Property,
+                                          unrealsdk::unreal::UUInt32Property,
+                                          unrealsdk::unreal::UUInt64Property,
+                                          unrealsdk::unreal::UFloatProperty,
+                                          unrealsdk::unreal::UDoubleProperty,
+                                          unrealsdk::unreal::UNameProperty,
+                                          unrealsdk::unreal::UArrayProperty,
+                                          unrealsdk::unreal::UBoolProperty,
+                                          unrealsdk::unreal::UInterfaceProperty,
+                                          unrealsdk::unreal::UObjectProperty,
+                                          unrealsdk::unreal::UStrProperty,
+                                          unrealsdk::unreal::UStructProperty,
+                                          unrealsdk::unreal::UClassProperty>;
+
+}  // namespace
+
+template <typename itype>
+struct polymorphic_type_hook<
+    itype,
+    detail::enable_if_t<std::is_base_of<unrealsdk::unreal::UObject, itype>::value>> {
+    template <int i = 0>
+    static const void* get(const unrealsdk::unreal::UObject* src, const std::type_info*& type) {
+        if (src == nullptr) {
+            type = nullptr;
+            return src;
+        }
+
+        if constexpr (i >= std::tuple_size_v<known_uobject_children>) {
+            // Fallback to object
+            type = &typeid(unrealsdk::unreal::UObject);
+            return src;
+        } else {
+            using cls = std::tuple_element_t<i, known_uobject_children>;
+
+            if (src->Class->Name == unrealsdk::unreal::cls_fname<cls>()) {
+                type = &typeid(cls);
+                return src;
+            }
+
+            return get<i + 1>(src, type);
+        }
+    }
+};
+
+namespace detail {
+
+// Allow FNames to cast to/from python strings
 template <>
 struct type_caster<unrealsdk::unreal::FName> {
    public:
@@ -33,6 +110,9 @@ struct type_caster<unrealsdk::unreal::FName> {
         return PyUnicode_FromStringAndSize(name.c_str(), static_cast<py::ssize_t>(name.size()));
     }
 };
-}  // namespace pybind11::detail
+
+}  // namespace detail
+
+}  // namespace pybind11
 
 #endif /* PYUNREALSDK_TYPE_CASTERS_H */
