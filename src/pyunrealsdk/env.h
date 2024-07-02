fdbda4071f5601799eb4f40da6f2543c53d7a841
apple1417 <git@apple1417.dev>
2024-07-02 21:30:45 +1200

    add `PYUNREALSDK_PYEXEC_ROOT` env var

--- src/pyunrealsdk/env.h
+++ src/pyunrealsdk/env.h
@@ -13,11 +13,13 @@ using unrealsdk::env::get_numeric;
 
 const constexpr env_var_key INIT_SCRIPT = "PYUNREALSDK_INIT_SCRIPT";
 const constexpr env_var_key DEBUGPY = "PYUNREALSDK_DEBUGPY";
+const constexpr env_var_key PYEXEC_ROOT = "PYUNREALSDK_PYEXEC_ROOT";
 
 namespace defaults {
 
 const constexpr auto INIT_SCRIPT = "__main__.py";
 // DEBUGPY - defaults to empty string (only used in defined checks)
+// PYEXEC_ROOT - defaults to empty string (cwd)
 
 }  // namespace defaults
 
